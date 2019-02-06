920e049e1735fba21402ea0828cc5e635b427940
Tsubasa Kato <tsubasa@superai.online>
2019-02-06 11:17:07 +0900

    modified some parts

--- loop-recursion.py
+++ loop-recursion.py
@@ -1,3 +1,5 @@
+#(C)Tsubasa Kato 2/6/2019
+#Written for study of loops and recursion.
 import random
 
 i = 1
@@ -6,18 +8,19 @@ rand_num = (random.randrange(10))
 print('The Random Number Picked was:')
 print(rand_num)
 
-while i < rand_num:
+while i <= rand_num:
 	#print("Random Number:")	
 	#print(rand_num)
 	print("Current Number:")
 	print(i)
 	i = i + 1
 	#to continue this script with more iterations of loop
-print('This time, it ended in') 
-print(i)
+print('This time, it ended in:') 
+result_num = i - 1
+print(result_num)
 print('times')
 
-print("Recursion Test Starting")
+print("Recursion Test Starting:")
 
 #recursive function to calculate factorial
 #Referenced: http://www.trytoprogram.com/python-programming/python-recursive-function/
