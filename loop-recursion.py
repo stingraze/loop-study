3caaf649f36856fc58dc390dfbb4b282be685b74
Tsubasa Kato <tsubasa@superai.online>
2019-02-06 11:11:51 +0900

    loop recursion (factorial) test
    Recursion (Factorial Test). Look at URL inside sourcecode for referenced URL.

---
+++ loop-recursion.py
@@ -0,0 +1,32 @@
+import random
+
+i = 1
+rand_num = (random.randrange(10))
+
+print('The Random Number Picked was:')
+print(rand_num)
+
+while i < rand_num:
+	#print("Random Number:")	
+	#print(rand_num)
+	print("Current Number:")
+	print(i)
+	i = i + 1
+	#to continue this script with more iterations of loop
+print('This time, it ended in') 
+print(i)
+print('times')
+
+print("Recursion Test Starting")
+
+#recursive function to calculate factorial
+#Referenced: http://www.trytoprogram.com/python-programming/python-recursive-function/
+
+def recursion(n):
+  if n == 1:
+    return 1
+  else:
+    print ("n:", n)
+    return (n * recursion(n-1))
+
+print (recursion(100))
