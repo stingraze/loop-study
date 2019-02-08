cfd084aca2d5e8d0eba9f32b235e57845b5ed328
Tsubasa Kato <tsubasa@superai.online>
2019-02-08 09:58:45 +0900

    Updated loop-recursion.py into another file
    Created a new study file with Python (3)

---
+++ loop-recursion2.py
@@ -0,0 +1,42 @@
+#(C)Tsubasa Kato 2/7/2019
+#Written for study of loops and recursion.
+import random
+
+i = 1
+rand_num = (random.randrange(10))
+
+print('The Random Number Picked was:')
+print(rand_num)
+
+while i <= rand_num:
+	#print("Random Number:")	
+	#print(rand_num)
+	print("Current Number:")
+	print(i)
+	i = i + 1
+	#to continue this script with more iterations of loop
+print('This time, it ended in:') 
+result_num = i - 1
+print(result_num)
+print('times')
+
+print("Recursion Test Starting:")
+
+#recursive function to calculate factorial
+#Referenced: http://www.trytoprogram.com/python-programming/python-recursive-function/
+
+
+def recursion(n):
+	global divider
+
+	if n == 100:
+		divider = n / 5
+	if n == 1:
+    		return 1
+	else:
+    		print ("n:", n, end = ' ')
+	if n == divider:
+		print('####### DIVIDED ######', end = '\n\n' )
+	return (n * recursion(n-1))
+
+print (recursion(100))
